74251307a0c36225ed9e1b9c86a4a485f2acf0a0
Rain1047 <3487851868@qq.com>
2024-05-21 22:42:30 +0800

    修改config
    Signed-off-by: Rain1047 <3487851868@qq.com>

--- _service_center/_llm_service/openaiTest.py
+++ _service_center/_llm_service/openaiTest.py
@@ -1,21 +1,21 @@
-from openai import OpenAI
-import os
-import time
-
-from _utils.config_util import get_config
-
-
-if __name__ == '__main__':
-  config = get_config()
-  client = OpenAI(api_key=config['openaiApiKey'])
-  # Set a delay between API requests
-  time.sleep(2)  # Add a delay of 2 seconds between requests
-
-  completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-    ]
-  )
-
-  print(completion.choices[0].message)
+# from openai import OpenAI
+# import os
+# import time
+#
+# from _utils.config_util import get_config
+#
+#
+# if __name__ == '__main__':
+#   config = get_config()
+#   client = OpenAI(api_key=config['openaiApiKey'])
+#   # Set a delay between API requests
+#   time.sleep(2)  # Add a delay of 2 seconds between requests
+#
+#   completion = client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#       {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+#     ]
+#   )
+#
+#   print(completion.choices[0].message)
